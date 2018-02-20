baf3fed53b5fca8a30d40c7419080d1560237b9d
Melissa Mcewen <mgmcewen@gmail.com>
2018-02-20 15:01:03 -0600

    reverting index

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import Nav from '../components/nav'
-import Footer from '../components/footer'
 
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
@@ -18,7 +17,6 @@ export default class TemplateWrapper extends React.Component {
   render() {
     return (
       <div>
-        <Nav /> 
         <div className="container">
             {this.props.children()}
         </div>
---
+++ src/utils/typography.js
@@ -0,0 +1,29 @@
+import Typography from "typography"
+
+const typography = new Typography({
+  baseFontSize: '18px',
+  baseLineHeight: 1.45,
+  googleFonts: [
+  {
+    name: 'VT323',
+    styles: [
+      'regular',
+    ],
+  },
+  {
+    name: 'Merriweather',
+    styles: [
+      '400',
+      '400i',
+      '700',
+      '700i',
+    ],
+  },
+],
+  headerFontFamily: ['VT323', "comic sans ms", 'Avenir Next', 'Helvetica Neue', 'Segoe UI', 'Helvetica', 'Arial', 'sans-serif'],
+  bodyFontFamily: ['Lucida Console', 'Monaco', 'monospace'],
+  // See below for the full list of options.
+})
+
+
+export default typography
